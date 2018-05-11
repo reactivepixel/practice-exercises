58dbba3f56d18c2e0a9f8a8a464238d69409c892
Karl Rohr <kjrohr@fullsail.edu>
2018-05-10 23:30:08 -0400

    Added second assignment for pool

---
+++ pool_assignment2/pool_assignment2/Employee_3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// The next exercise would be to create a loop prompting the user for how many employees they would like create.
+// 1.	Move logic around to fit in a loop
+// 2.	Prompt user for a number of employees.
+// 3.	Prompt the user for an employee name, employer name, occupation, and hourly rate.
+// 4.	Display all the employees. 
+
+
+namespace pool_assignment2
+{
+    class Employee_3
+    {
+        public Employee_3(string name, string employer, string occupation, double hourlyRate)
+        {
+            this.name = name;
+            this.employer = employer;
+            this.occupation = occupation;
+            this.hourlyRate = hourlyRate;
+            this.hoursWorked = 0.00;
+        }
+        public string name { get; set; }
+        public string employer { get; set; }
+        public string occupation { get; set; }
+        public double hourlyRate { get; set; }
+        public double hoursWorked { get; set; }
+
+        public void display()
+        {
+            Console.WriteLine("**********************************************************************");
+            Console.WriteLine("Employee:");
+            Console.WriteLine("Name: " + this.name);
+            Console.WriteLine("Employer: " + this.employer);
+            Console.WriteLine("Hourly Rate: " + this.hourlyRate);
+            Console.WriteLine("Hours Worked: " + this.hoursWorked);
+            Console.WriteLine("**********************************************************************");
+        }
+    }
+}
---
+++ pool_assignment2/pool_assignment2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pool_assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ***************************************************************************
+            // Exercise 1
+            Employee_1 myEmployee = new Employee_1("Bob");
+            myEmployee.display();
+            // ***************************************************************************
+
+            // ***************************************************************************
+            // Exercise 2
+            Employee_2 myEmployee2 = new Employee_2("Same", "Jared", "Sales Associate", 8.50);
+            myEmployee2.display();
+            // ***************************************************************************
+
+            // ***************************************************************************
+            // Exercise 3
+            Console.WriteLine("Please enter how many employees you would like to create.");
+            int numberOfEmployees = Convert.ToInt32(Console.ReadLine());
+            List<Employee_3> employees = new List<Employee_3>();
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine("Please enter a name for the employee.");
+                string employeeName = Console.ReadLine();
+                Console.WriteLine("Please enter a name for " + employeeName + "'s employer.");
+                string employerName = Console.ReadLine();
+                Console.WriteLine("Please enter an occupation for " + employeeName + ".");
+                string occupation = Console.ReadLine();
+                Console.WriteLine("How much does " + employeeName + " make per hour?");
+                double hourlyRate = Convert.ToDouble(Console.ReadLine());
+                Employee_3 newEmployee = new Employee_3(employeeName, employerName, occupation, hourlyRate);
+                employees.Add(newEmployee);
+            }
+            for (int i = 0; i < employees.Count; i++)
+            {
+                employees[i].display();
+            }
+            // ***************************************************************************
+
+            // ***************************************************************************
+            // Exercise 4
+            Console.WriteLine("Please enter how many employees you would like to create.");
+            int numOfEmployees = Convert.ToInt32(Console.ReadLine());
+            List<Employee_4> employeeList = new List<Employee_4>();
+            for (int i = 0; i < numOfEmployees; i++)
+            {
+                Console.WriteLine("Please enter a name for the employee.");
+                string employeeName = Console.ReadLine();
+                Console.WriteLine("Please enter a name for " + employeeName + "'s employer.");
+                string employerName = Console.ReadLine();
+                Console.WriteLine("Please enter an occupation for " + employeeName + ".");
+                string occupation = Console.ReadLine();
+                Console.WriteLine("How much does " + employeeName + " make per hour?");
+                double hourlyRate = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("How many hours did " + employeeName + " work this week?");
+                double hoursWorked = Convert.ToDouble(Console.ReadLine());
+                Employee_4 theNewEmployee = new Employee_4(employeeName, employerName, occupation, hourlyRate, hoursWorked);
+                employeeList.Add(theNewEmployee);
+            }
+
+            for (int i = 0; i < employeeList.Count; i++)
+            {
+                employeeList[i].display();
+            }
+
+            // ***************************************************************************
+
+        }
+    }
+}
