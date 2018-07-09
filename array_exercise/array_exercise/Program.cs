06b0f8cd1f5030bd271255978b969dece0f7b684
Karl Rohr <kjrohr@student.fullsail.edu>
2018-07-09 13:40:47 -0400

    Added in rest of weeks assignments. Some still need readmes.

---
+++ array_exercise/array_exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace array_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array of at least 5 values
+            int[] myArray = new int[5];
+            // Loop through the array to assign values to each index.
+            for (int i = 0; i < myArray.Length; i++) 
+            {
+                // Prompt user for values
+                Console.WriteLine("Please enter an integer.");
+                int input = Convert.ToInt32(Console.ReadLine());
+                myArray[i] = input;
+                Console.WriteLine("Iteration: " + i + ". Input: " + input);
+            }
+            // Give a space inbetween outputs.
+            Console.WriteLine();
+
+            // Reverse the array
+            Array.Reverse(myArray);
+
+            // Loop through the array again and display it.
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
---
+++ array_exercise/readme.md
@@ -0,0 +1,20 @@
+---
+title: Looping with Collections
+difficulty: 2
+degree: Central
+outcome: Nulll
+categories:
+  - C#
+  - Collections
+  - Loops
+---
+
+`Challenge`
+
+```
+1. Please create an array with at least 5 elements.
+2. Loop through the array. On each iteration prompt the user to input a value and add it to the array.
+3. While in the loop, display the current iteration and the value input.
+4. Reverse the array.
+5. Display the array again.
+```
