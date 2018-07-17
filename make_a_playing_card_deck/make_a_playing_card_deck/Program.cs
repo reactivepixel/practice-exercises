a8a0fa05bca1f80a9e14464e9a286cebc5a1a229
Karl Rohr <kjrohr@student.fullsail.edu>
2018-07-16 22:37:01 -0400

    Added in the start of a series for standard playing card challenges.

---
+++ make_a_playing_card_deck/make_a_playing_card_deck/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace make_a_playing_card_deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a collection that will act as a standard playing card deck that could be used for games. You need to be add items to it, take items out of it, and randomize it.
+            List<Card> deck = new List<Card>();
+           // A playing card deck has 52 cards in it, 4 suits, 13 cards per suit
+           for (int i = 0; i < 52; i++)
+            {
+                // Prompt the user to Create the deck
+                Console.WriteLine("Enter a suit: 1 for Hearts | 2 for Diamonds | 3 for Clubs | 4 for Spades");
+                string suit = Console.ReadLine();
+                if (suit == "1")
+                {
+                    suit = "Hearts";
+                }
+                else if (suit == "2")
+                {
+                    suit = "Diamonds";
+                }
+                else if (suit == "3")
+                {
+                    suit = "Clubs";
+                }
+                else
+                {
+                    suit = "Spades";
+                }
+                Console.WriteLine("Enter a value: 1-14. Please be mindful that you don't duplicate a value for a suit.");
+                int value = Convert.ToInt32(Console.ReadLine());
+                // Make an instance of the card class.
+                Card card = new Card(suit, value);
+                Console.WriteLine("Card: " + card.value + " of " + card.suit + " created.");
+                // Add the card to the deck.
+                deck.Add(card);
+            }
+        }
+    }
+}
---
+++ make_a_playing_card_deck/readme.md
@@ -0,0 +1,24 @@
+---
+title: Make a standard playing card deck.
+difficulty: 3
+degree: Central
+outcome: Nulll
+categories:
+  - C#
+  - Classes
+  - Objects
+  - Loops
+  - Collections
+---
+
+`Challenge 1`
+
+```
+1. Set up a collection that can be used like a deck. I.e you can add cards to it, remove cards from it, and shuffle it.
+2. Create a Card class. It should have 2 properties, suit and value.
+3. A standard playing card deck has 52 cards, 4 suits, and 13 cards per suit.
+4. Loop through your collection and prompt the user to create each card.
+5. Inside that loop display each card as the user creates them.
+6. At the end of the loop  Add the card to the collection.
+
+```
